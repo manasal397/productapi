5349ce434b2aaacf6dc354e397fdc2e0ea90966a
manasal397 <59077105+manasal397@users.noreply.github.com>
2020-01-06 13:45:18 +0530

    Update order.router.js

--- router/order.router.js
+++ router/order.router.js
@@ -46,5 +46,47 @@ orderRoute.route('/:username').get(function(req, res) {
 });
 
 orderRoute.route('/:id').patch(function(req, res) {
-
+let id = req.params.id;
+ // validate request
+ if(!re.body.orderstatus) {
+  return res.status(400).send ({
+   message: "Order content cannot be empty"
+  });
+ }
+ 
+ //find note and update it with the request body
+ Order.findByIdAndUpdate(id, {
+  orderstatus: req.body.orderstatus || "Accepted"
+ }, {new: true})
+ .then(obj => {
+  if(!obj) {
+   return res.status(404).send({
+    message: "Order not found with id " + id
+   });
+  }
+  res.send(obj);
+ }).catch(err => {
+  if(err.kind === 'id') {
+   return res.status(404).send({ 
+    message: "Order not found with id " + id
+   });
+  }
+  return res.status(500).send({
+   message: "Error updating note with id " + id
+  })'
+ });
 });
+
+orderRoute.route('/:id').delete(function(req, res) { 
+ let id = req.params.id;
+ 
+ Order.findByIdAndDelete({ _id: id }, function(err, data) {
+  if(err) {
+   console.log(err);
+  } else {
+   res.status(200).json({ message: "Successfully deleted the order" });
+  }
+ });
+});
+
+module.exports = orderRoute;
